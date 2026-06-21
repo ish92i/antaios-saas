09b3b76e614ebb7d30a2ca2516e2c34a6f0a3fef
flip-flap-leni <flip.flap.leni@gmail.com>
2026-06-21 13:43:50 +0200

    refactor: remove Stripe errors, simplify types

--- types.ts
+++ types.ts
@@ -1,9 +1,6 @@
 import { Doc } from "~/convex/_generated/dataModel";
-import { PlanKey } from "~/convex/schema";
 
 export type User = Doc<"users"> & {
   avatarUrl?: string;
-  subscription?: Doc<"subscriptions"> & {
-    planKey: PlanKey;
-  };
+  subscription?: Doc<"subscriptions">;
 };
